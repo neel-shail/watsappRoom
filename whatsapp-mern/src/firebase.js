3aa183d7768b6ba4f24377b260f498146d656fbd
neel-shail <shaileshsingh071@gmail.com>
2023-05-07 20:59:04 +0530

    code first commit

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDl7DXx3urSMkB-2qwpRvpsx5ZrIpgYH9g",
+    authDomain: "whatsapp-mern-fce80.firebaseapp.com",
+    projectId: "whatsapp-mern-fce80",
+    storageBucket: "whatsapp-mern-fce80.appspot.com",
+    messagingSenderId: "802485322454",
+    appId: "1:802485322454:web:df56c5b3f17f1c1ca9ff77"
+  };
